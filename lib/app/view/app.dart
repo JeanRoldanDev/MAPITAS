6952f7bd11d0a6474d044383310e95fe16e07fa7
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2022-04-03 16:55:49 -0500

    princiapal page

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -6,9 +6,7 @@
 // https://opensource.org/licenses/MIT.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:mapitas/counter/counter.dart';
-import 'package:mapitas/l10n/l10n.dart';
+import 'package:mapitas/principal/principal.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -22,12 +20,7 @@ class App extends StatelessWidget {
           accentColor: const Color(0xFF13B9FF),
         ),
       ),
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: Principal(),
     );
   }
 }
--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mapitas/app/app.dart';
-import 'package:mapitas/counter/counter.dart';
+import 'package:mapitas/principal/principal.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(Principal), findsOneWidget);
     });
   });
 }
